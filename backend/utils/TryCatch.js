e24dc90ba9484cf0099cd8fe6bd8ba6e36830c84
Gitesh-Holkar <giteshholkar123@gmail.com>
2025-01-21 20:37:56 +0530

    Update TryCatch.js

--- backend/utils/TryCatch.js
+++ backend/utils/TryCatch.js
@@ -1,7 +1,6 @@
 const TryCatch = (handler) =>{
     return async (req,res,next)=> {
         try {
-            console.log(req);
             await handler(req,res,next);
         } catch (error) {
                 res.status(500).json({
